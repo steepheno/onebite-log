af0d5e38dd59a203d63aff7501a84fe4554f04a3
Joonsun Ryu <rjs7289@gmail.com>
2025-11-19 22:21:22 +0900

    이메일을 통한 회원가입, 로그인 구현

---
+++ src/api/auth.ts
@@ -0,0 +1,33 @@
+import supabase from "@/lib/supabase";
+
+export async function signUp({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
