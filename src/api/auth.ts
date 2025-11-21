9cbdbeaff16ad56210bf0f1bd636efff01c2a8d9
Joonsun Ryu <rjs7289@gmail.com>
2025-11-21 23:25:36 +0900

    비밀번호 재설정 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,7 @@
 import supabase from "@/lib/supabase";
 import type { Provider } from "@supabase/supabase-js";
 
+/* 회원가입 */
 export async function signUp({
   email,
   password,
@@ -17,6 +18,7 @@ export async function signUp({
   return data;
 }
 
+/* 비밀번호 로그인 */
 export async function signInWithPassword({
   email,
   password,
@@ -33,9 +35,30 @@ export async function signInWithPassword({
   return data;
 }
 
+/* OAuth 로그인 */
 export async function signInWithOAuth(provider: Provider) {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+/* 비밀번호 초기화 */
+export async function requestPasswordResetEmail(email: string) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+/* 비밀번호 재설정 */
+export async function updatePassword(password: string) {
+  const { data, error } = await supabase.auth.updateUser({
+    password,
   });
 
   if (error) throw error;
