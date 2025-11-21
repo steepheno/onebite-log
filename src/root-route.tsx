1d19df407d04f3ea196277fafcfddf6557fd86fb
Joonsun Ryu <rjs7289@gmail.com>
2025-11-21 21:42:43 +0900

    라우트 가드 설정 구현

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -7,19 +7,27 @@ import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
 import GlobalLayout from "./components/layout/global-layout";
+import GuestOnlyLayout from "./components/layout/guest-only-layout";
+import MemberOnlyLayout from "./components/layout/member-only-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SigninPage />} />
-        <Route path="/sign-up" element={<SignupPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        {/* 로그인하지 않은 사용자도 접근 가능 */}
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SigninPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        {/* 로그인한 사용자만 접근 가능 */}
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
